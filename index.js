07e6907c8e3ed368f6683a309441c1dad6c52b23
sandeep <sandeepmukkadan@gmail.com>
2018-08-08 21:03:32 +0530

    google-oAuth initial

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const Google = require('passport-google-oauth20');
+const GoogleStrategy = Google.Strategy
+const keys =    require('./configs/keys');
+
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send({'hi': 'there'});
-})
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googlClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, cb) => {
+        console.log("getAccess",accessToken);
+    }
+));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
