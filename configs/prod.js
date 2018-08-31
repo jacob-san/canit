04af744d9a05d05ca6c3330e47657385700275e3
San <jacobsan.dev@gmail.com>
2018-08-31 08:54:11 +0530

    add credits to User model

--- configs/prod.js
+++ configs/prod.js
@@ -1,4 +1,3 @@
-// dev.js Dont commit this
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googlClientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,7 +1,13 @@
 const keys = require('../configs/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-    console.log(req.body);
+  app.post('/api/stripe', async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id,
+    })
+    console.log("charge",charge);
   })
 };
