e55987c9129a5221c8fe95831fda1de076922079
San <jacobsan.dev@gmail.com>
2018-09-02 21:40:04 +0530

    wired up mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,25 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-  
+  app.post('/api/surveys', requireLogin, requireCredits, () => {
+      const { title, subject, body, recipients } = req.body;
+
+      const survey = new Survey({
+          title,
+          body,
+          subject,
+          recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+          _user: req.user.id,
+          dateSent: Date.now(),
+      })
+
+      const mailer = new Mailer(survey, surveyTemplate(survey));
+      mailer.send();
+  })
 }
